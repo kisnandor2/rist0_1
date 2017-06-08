d5478a5f5ee070f2ff9318cbaf266d1d84597be7
kisnandor2 <kisnandor3@gmail.com>
2017-06-08 12:51:26 +0300

    3.3 done. Bonus needed

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,20 +1,26 @@
 from createImage import *
-from dummyData import *
+import operator
+# from dummyData import *
 
 def ftd(imageMatrix):
 	#First time element is displaced
 	n = len(imageMatrix[0])
+	m = len(imageMatrix)
 	ret = [0]*n
 	for i in range(0, n):
 		moved = False
-		for j in range(1, len(imageMatrix)):
+		for j in range(1, m):
 			if imageMatrix[j-1][i] != imageMatrix[j][i]:
 				ret[i] = j
 				moved = True
 			if moved:
 				break
+	# print(imageMatrix[0])
+	# print(ret)
 	ret = [x for (y,x) in sorted(zip(imageMatrix[0],ret))] #Sort ret by First row of matrix
 	# print(sorted(imageMatrix[0]))
+	if Order.checkDecreasing(imageMatrix[m-1]):
+		ret.reverse();
 	return ret
 
 def ftp(imageMatrix):
@@ -35,18 +41,20 @@ def ftp(imageMatrix):
 
 def nit(ftpList):
 	#Number of steps to reach final state
-	for i in range(max(ftpList), 41):
-		if imageMatrix[i] != imageMatrix[i+1]:
-			print("ALMA")
 	return max(ftpList)
 
+def rpa(ftdList, ftpList):
+	return sum(list(map(operator.sub, ftpList, ftdList)))
+
 def main():
-	arg = 2
+	arg = 5
 	data = loadData("data.csv", arg)
 	imageMatrix = formatDataIntoMatrix(data)
-	print(ftd(imageMatrix))
-	print(ftp(imageMatrix))
-	print(nit(ftp(imageMatrix)))
+	_ftd = ftd(imageMatrix)
+	_ftp = ftp(imageMatrix)
+	_nit = nit(_ftp)
+	_rpa = rpa(_ftd, _ftp)
+	print("{0}\n{1}\n{2}\n{3}".format(_ftd, _ftp, _nit, _rpa))
 
 if __name__ == "__main__":
 	main()
