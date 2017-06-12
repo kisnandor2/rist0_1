a916a8d30eff9b425747f86665e0d0ae38fb1b85
kisnandor2 <kisnandor3@gmail.com>
2017-06-12 15:15:20 +0300

    Readme added

---
+++ readMe.py
@@ -0,0 +1,11 @@
+print("\nI've tried tried my best, however I was not able to implement my own classification function")
+print("I am not sure how can I use the ftd, ftp, etc. to build a decision tree or similar :/")
+print("\nHowever I have:")
+
+print("\nreadData.py - 3.1 - loading and looking at the data, pirntSampleAndLabelNumber")
+print("\ncreateImage.py imageIndex - 3.2 - sample inspection, printing image, checkingOrder")
+print("\nfeatureExtraction.py imageIndex - 3.3 - ftd, ftp, nit, rpa computed for image, printed")
+print("\nhistograms.py - 3.4 - creating nit and rpa histograms, printed. Bonus: ftd, ftp avg, stdDev computed")
+print("\ndataSplitting.py - 3.5 - splitting the data into Training and Validation subset(no main function)")
+print("\npredict.py - 3.6, 3.7 - scikit-learn used to train and fit the data, classification report and confusion matrix printed")
+print("\n")
